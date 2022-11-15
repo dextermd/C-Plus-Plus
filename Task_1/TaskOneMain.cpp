87e36b2774cd9065ef02fc35addf8d08cd510f6b
DEXTER MD <dextermdpay@gmail.com>
2022-11-15 09:02:21 +0200

    Add project files.

---
+++ Task_1/TaskOneMain.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+using namespace std;
+
+void main() 
+{
+	cout << endl << endl;
+
+	cout << "\t      \"Numbers\"\n";
+	cout << "Number \\ONE\\ – this is the sun.\n";
+	cout << "Number \\TWO\\ – this is my shoe.\n";
+	cout << "Number \\THREE\\ – this is a tree.\n";
+	cout << "Number \\FOUR\\ – cats at the door.\n";
+	cout << "Number \\FIVE\\ – I like this pie.\n";
+	cout << "Number \\SIX\\ – this is a stick.\n";
+	cout << "Number \\SEVEN\\ – I see a raven.\n";
+	cout << "Number \\EIGHT\\ – take this plate.\n";
+	cout << "Number \\NINE\\ – I am fine.\n";
+	cout << "Number \\TEN\\ – this is a pen.\n\n";
+	cout << "\t\\\\ Author unknown\\\\";
+
+	cout << endl << endl;
+
+	_getch();
+
+}
---
+++ Task_3/TaskThreeMain.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+using namespace std;
+
+void main()
+{
+	cout << endl << endl;
+
+	cout << "\t\t\\\\My first class-work\\\\\n\n";
+
+	cout << " =============================================================== \n";
+	cout << "|\t\t|\t\t|\t\t|\t\t\|\t\t \n";
+	cout << "|      \"Name\"\t|   \"Quantity\"\t|    \"Price\"\t|    \"Value\"\t\|\n";
+	cout << "|\t\t|\t\t|\t\t|\t\t\|\t\t \n";
+	cout << "|---------------------------------------------------------------|\n";
+	cout << "|\t1\t|\t2\t|\t3\t|\t4\t|\t \n";
+	cout << "|===============================================================|\n";
+	cout << "|\t\t|\t\t|\t\t|\t\t\|\n";
+	cout << "|\t\t|\t\t|\t\t|\t\t\|\n";
+	cout << "|---------------------------------------------------------------|\n";
+	cout << "|\t\t|\t\t|\t\t|\t\t\|\n";
+	cout << "|\t\t|\t\t|\t\t|\t\t\|\n";
+	cout << " =============================================================== \n";
+
+	cout << endl << endl;
+
+	_getch();
+}
