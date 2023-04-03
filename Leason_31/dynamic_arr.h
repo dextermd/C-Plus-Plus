39ce9edc014b3236466afb470e3a2da455b487dd
DEXTERMD <dextermdpay@gmail.com>
2023-04-03 09:04:02 +0300

    Add project files.

---
+++ Leason_31/dynamic_arr.h
@@ -0,0 +1,19 @@
+#pragma once
+int* memory(int size);
+void init(int* m, int size, int = 1, int b = 10);
+void showDynamArr(int* m, int size);
+void destroy(int*& m);
+int* mas_odd(int* m, int size, int& size2);
+int* push_back(int* m, int size, int &size2, int value);
+int* push_start(int* m, int size, int &size2, int value);
+void mas_odd(int*& m, int& size);
+void push_back(int*& m, int& size, int value);
+void add_by_index(int*& m, int& size, int index, int value);
+
+//-------------------------
+int** memory(int row, int col);
+void init(int** m, int row, int col, int a = 1, int b = 10);
+void show(int** m, int row, int col);
+void destroy(int**& m, int row);
+
+void add_end_str(int**& m, int& row, int col, int* add);
