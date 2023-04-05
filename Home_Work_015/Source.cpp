b74cbde308b351d014897f59f0cc1e5a0ee01960
DEXTERMD <dextermdpay@gmail.com>
2023-04-05 23:04:15 +0300

    Finish

--- Home_Work_015/Source.cpp
+++ Home_Work_015/Source.cpp
@@ -1,9 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
 #include <stdlib.h>
 #include <conio.h>
 #include <iomanip>
 #include <Windows.h>
 #include <string.h>
+#include <string>
 
 using namespace std;
 
@@ -13,7 +16,8 @@ using namespace std;
 int main()
 {
 
-#if 1
+
+#if 0
 
     // --------------------------------------------------------------------------------------------------------------- \\
     //  Задание 1: Дан массив строк (проинициализирован). Вывести массив на экран, после каждой
@@ -28,19 +32,18 @@ int main()
     //  Строка максимальной длины : Вашингтон(9), порядковый номер : 5
 
     const int ROW = 5, COL = 30;
-
+    int max = 0, index = 0;
     char str[ROW][COL] = { 
                             "Kishinev",
                             "Bucharest",
+                            "Washington",
                             "Kyiv",
-                            "Rome",
-                            "Washington"
+                            "Rome",     
     };
 
     for (int i = 0; i < ROW; i++)
     {
         int g = 0;
-        int max = 0;
 
         for (int j = 0; str[i][j]; j++)
         {
@@ -48,7 +51,7 @@ int main()
             {
                 g++;
             }
-            max = g;
+
             max < g ? max = g : max;
             cout << str[i][j];
         }
@@ -57,12 +60,16 @@ int main()
 
     for (int i = 0; i < ROW; i++)
     {
-        cout << str[i] << " ";
+        if (max == strlen(str[i])) {
+
+            cout << "Строка максимальной длины : " << str[i] << "(" << max << ")" << ", порядковый номер : " << i + 1;
+        }
+
     }
 
 #endif
 
-#if 1
+#if 0
 
     //    Задание 2: Дан массив строк.Напишите программу, которая выводит его на экран, указав после
     //    каждого слова строки в скобках его длину, а ниже каждой строки вывести :
@@ -73,17 +80,19 @@ int main()
     // 
     //    Например, исходный массив строк :
     //      My name is Artur.
-    //      I am learning C ++ programming language.
+    //      I am learning C++ programming language.
     // 
     //    Пример работы программы :
     //      My(2) name(4) is(2) Artur(5).
     //      Количество слов в строке : 4
     //      Слово максимальной длины : Artur
     //      Слово минимальной длины : My
+    // 
     //      I(1) am(2) learning(8) C++(3) programming(11) language(8)
     //      Количество слов в строке : 6
     //      Слово максимальной длины : programming
     //      Слово минимальной длины : I
+    // 
     //    * Можно добавить вывод всех слов минимальной и максимальной длины в текущей строке,
     //    если таковые есть.Например :
     // 
@@ -91,10 +100,41 @@ int main()
     //      Слово максимальной длины : Artur
     //      Слова минимальной длины : My, is
 
+    const int ROW = 2, COL = 50;
+    char str[ROW][COL] = { "My name is  Artur.", "I am learning C++ programming language." };
+    int num = 0, value = 1, value2 = 1;
+    char min[20], max[20];
+    for (int i = 0; i < ROW; i++)
+    {
+        num = 0;
+
+        for (int j = 0; str[i][j]; j++)
+        {
+            str[i][j] == '.' ? str[i][j] = ' ' : str[i][j];
+        }
+
+        char* token = strtok(str[i], " ");
+        strcpy(min, token);
+        while (token != NULL) {
+            value2 >= strlen(token) ? value2 = strlen(token), strcpy(min, token) : token;
+            value <= strlen(token) ? value = strlen(token), strcpy(max, token) : token;
+            
+            num++;
+            cout << token << "(" << strlen(token) << ") ";
+
+            token = strtok(NULL, " ");
+
+         }
+        
+        cout << endl;
+        cout << "Количество слов в строке : " << num << endl;;
+        cout << "Слово максимальной длины : " << max << endl;;
+        cout << "Слово минимальной длины : " << min << endl;;
+        cout << endl;
+    }
+
 
 #endif
-
-
 
 
 
